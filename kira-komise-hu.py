755cb7e6b0eed8c4bdf4c78ce65d4688a06087d7
Elroi-Yong-Tian-King <163367858+Elroi-Yong-Tian-King@users.noreply.github.com>
2024-04-05 08:36:29 +0800

    Update and rename kira-komise.py to kira-komise-ey.py 2

--- kira-komise-hu.py
+++ kira-komise-hu.py
@@ -1,9 +1,13 @@
+#Atur cara mengira komisen
+#Pengisytiharan pemboleh ubah
 jumlah=0
 jualan=0
 komisen=0
 ulang="Y"
 while ulang=="Y":
+     #Input
      jualan=float(input("Masukkan jumlah jualan: RM"))
+     #Proses
      if jualan>80:
            kadar_komisen=0.005
      elif jualan>70:
@@ -15,8 +19,11 @@ while ulang=="Y":
      else:
            kadar_komisen=0.02
      komisen=jualan*kadar_komisen
+     #Output
      print("Komisen anda ialah RM",round(komisen,2))
      jumlah=jumlah+komisen
+     #Input
      ulang=input("Masukkan Y untuk terusan pengiraan atau N untuk hentikan pengiraan:")
+#Output
 print("\n\tJumlah komisen ialah RM",round(jumlah,2))
 print("Sekian,terima kasih")
